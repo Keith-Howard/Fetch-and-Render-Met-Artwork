2c6b7d5aa5852b21a111fce5c3f1de8c1210c78e
Keith Howard <54951171+Keith-Howard@users.noreply.github.com>
2022-06-06 21:50:21 -0400

    Update getData.jsx
    additional refactor to improve code

--- getData.jsx
+++ getData.jsx
@@ -2,32 +2,35 @@ const { useEffect, useState } = React;
 
 function App() {
     const [query, setQuery] = useState('');
-    const [idUrl] = useState('https://collectionapi.metmuseum.org/public/collection/v1/search?q=');
-    const [artworkUrl] = useState('https://collectionapi.metmuseum.org/public/collection/v1/objects/');
+    const idUrl = 'https://collectionapi.metmuseum.org/public/collection/v1/search?q=';
+    const artworkUrl = 'https://collectionapi.metmuseum.org/public/collection/v1/objects/';
     const [posts, setPosts] = useState([]);
     const [userInput, setUserInput] = useState('');
     const [currentPage, setCurrentPage] = useState(1);
-    const [postsPerPage] = useState(50);
+    const postsPerPage = 50;
 
     useEffect(() => {
-        let artwork = [];
-        axios.get(idUrl + query)
-            .then(response => {
-                let objectIds = response.data.objectIDs;
-                for (let id of objectIds) {
-                    axios.get(artworkUrl + id)
-                        .then(response => { 
-                            artwork = [...artwork, response.data]
-                            setPosts(artwork)
-                        })
-                        .catch(err => {
-                            console.log(err);
-                        })
-                }
-            })
-            .catch(err => {
-                console.log(err);
-            })
+        if (query !=="") {
+            let searchString =  idUrl + query;
+            let artwork = [];
+            axios.get(searchString)
+                .then(response => {
+                    let objectIds = response.data.objectIDs;
+                    for (let id of objectIds) {
+                        axios.get(artworkUrl + id)
+                            .then(response => { 
+                                artwork = [...artwork, response.data]
+                                setPosts(artwork)
+                            })
+                            .catch(err => {
+                                console.log(err);
+                            })
+                    }
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+        }
     },[query])
 
     const Pagination = ({postsPerPage, totalPosts, paginate}) => {
@@ -35,14 +38,14 @@ function App() {
         for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
             pageNumbers.push(i);
         } 
-
+          
         return (
             <nav>
                 <ul className="pagination">
                     {pageNumbers.map(number =>(
                         <li key={number}>
-                            <button onClick={() => paginate(number)} key={number} id={number} href="#top" className={currentPage === number ? "btn btn-secondary" : 'btn btn-light'} >
-                                {number}
+                            <button onClick={() => paginate(number)} key={number} id={number} className={currentPage === number ? "btn btn-secondary" : 'btn btn-light'}>
+                            <a href="#top">{number}</a>
                             </button>
                         </li>
                     ))}
@@ -50,29 +53,25 @@ function App() {
             </nav>
         )
     }
-    const replaceImage =(array) => {
-        for (let item of array) {
-            if (item.primaryImage === "") {
-                item.primaryImage = "imageNA.png";
-            }
-        }
-    }
 
-    replaceImage(posts);
+    posts.map(item => {
+        if (item.primaryImage === "") {
+            item.primaryImage = "imageNA.png";
+        }
+    })
 
     const indexOfLastPost = currentPage * postsPerPage;
     const indexOfFirstPost = indexOfLastPost - postsPerPage;
     const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
     const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
-    
-    
-    const handleSubmit = () => {
+    const handleSubmit = (event) => {
         if (userInput === '') {
             alert('Please enter search criteria!');
         } else{
             setQuery(userInput);
         }
+        event.preventDefault();
         document.getElementById("userInputBox").value='';
     }
 
@@ -80,7 +79,7 @@ function App() {
         <form>
             <div>Browse the Collection</div>
             <input id='userInputBox' type='text' className="inputStyle" onChange={(event) => setUserInput(event.target.value)} placeholder="Search..."/>
-            <input type='submit' onClick={() => handleSubmit()} className="inputStyle" />
+            <input type='submit' onClick={(event) => handleSubmit(event)} className="inputStyle" />
             <div>
                 <ul className="listContainer">
                     {currentPosts.map((item, i) => <li className='list-group-item' key={i} id={i}><img src={item.primaryImage} className="artworkImage"/><br/><a href={item.objectURL} target="_blank">{item.title}</a> By {item.artistDisplayName}<br/>{item.artistDisplayBio} </li>)}
